8b4cd49ef24c4155ce2ec3c1ab6070d502d644c0
igorbezsmertnyi <bezsmertnyi.igor@gmail.com>
2018-05-10 23:25:04 +0200

    add drawing functionality

--- client/src/app/components/area/area.component.ts
+++ client/src/app/components/area/area.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core'
+import { Component, Output, EventEmitter, ViewChild, ElementRef, Renderer2 } from '@angular/core'
 import { MousePosition } from '../../models/MousePosition'
+import { BoundingRect } from '../../models/BoundingRect'
 
 @Component({
   selector: 'app-area',
@@ -8,18 +9,47 @@ import { MousePosition } from '../../models/MousePosition'
 })
 
 export class AreaComponent {
+  proccessing: boolean = false
+  ctx: any = null
+  canvasPostion: BoundingRect
   mousePosition: MousePosition = {
     posX: 0,
     posY: 0
   }
 
+  @ViewChild('canv') canvas: ElementRef
   @Output() onMouseMove = new EventEmitter<MousePosition>()
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d')
+
+    this.renderer.setAttribute(this.canvas.nativeElement, 'width', window.innerWidth.toString())
+    this.renderer.setAttribute(this.canvas.nativeElement, 'height', window.innerHeight.toString())
+
+    this.canvasPostion = this.canvas.nativeElement.getBoundingClientRect()
+  }
 
   coordinates(e) {
-    this.mousePosition.posX = e.clientX
-    this.mousePosition.posY = e.clientY
+    this.mousePosition.posX = e.clientX - this.canvasPostion.left
+    this.mousePosition.posY = e.clientY - this.canvasPostion.top
     this.onMouseMove.emit(this.mousePosition)
+
+    this.drawing()
+  }
+
+  drawStart() { 
+    this.proccessing = true 
+    this.ctx.beginPath()
+  }
+
+  drawEnd() { this.proccessing = false }
+
+  private drawing() {
+    if (!this.proccessing) return
+
+    this.ctx.lineTo(this.mousePosition.posX, this.mousePosition.posY)
+    this.ctx.stroke()
   }
 }
---
+++ client/src/app/models/BoundingRect.ts
@@ -0,0 +1,10 @@
+export interface BoundingRect {
+  bottom: number
+  top: number
+  right: number
+  left: number
+  width: number
+  height: number
+  x: number
+  y: number
+}
